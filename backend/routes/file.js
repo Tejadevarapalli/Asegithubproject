ef8f926c72a02d02b425b19bcae77526423ab9cd
vasudeva <vmcpq@mail.umkc.edu>
2019-04-07 16:56:44 -0500

    vasu

--- backend/routes/file.js
+++ backend/routes/file.js
@@ -6,7 +6,10 @@ var path = require('path');
 
 var store = multer.diskStorage({
     destination : function(req,file,cb){
-        cb(null, './uploads');
+       // cb(null, './uploads');
+        cb(null,path.resolve(__dirname, './uploads'));
+        console.log(cb);
+        console.log(__dirname);
     },
     filename:function(req,file,cb){
         console.log(file);
@@ -20,8 +23,10 @@ var upload = multer({storage:store}).single('file');
 _router.post('/upload',function(req,res,next){
     upload(req,res,function(err){
         if(err){
+            console.log(upload);
             return res.status(501).json({error:err});
         }
+       // console.log( upload)
         //do all database record saving activity
         return res.json({originalname:req.file.originalname, uploadname:req.file.filename});
     });
