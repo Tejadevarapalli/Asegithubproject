a03d2ffcc076f11ac5ff2bb8b1a3505e782e06fc
vasudeva <vmcpq@mail.umkc.edu>
2019-04-14 18:12:10 -0500

    frontend
    projects list

--- backend/frontend/src/app/mymodels/mymodels.component.ts
+++ backend/frontend/src/app/mymodels/mymodels.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {mongoService} from "../mongo.service";
+import {IProject} from "./project";
 
 @Component({
   selector: 'app-mymodels',
@@ -7,12 +8,40 @@ import {mongoService} from "../mongo.service";
   styleUrls: ['./mymodels.component.css']
 })
 export class MymodelsComponent implements OnInit {
+  pageTitle = 'Projects List';
 
-  constructor(public getDetails: mongoService) { }
+  listFilter1 = '';
+  get listFilter(): string {
+    return this.listFilter1;
+  }
+  set listFilter(value: string) {
+    this.listFilter1 = value;
+    this.filteredproject = this.listFilter ? this.performFilter(this.listFilter) : this.projects;
+  }
+
+  filteredproject: IProject[] = [];
+  projects: any;
+
+  performFilter(filterBy: string): IProject[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.projects.filter((project: IProject) =>
+      project.Projecttitle.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  constructor(public getDetails: mongoService) {
+    this.filteredproject = this.projects;
+  }
 
   ngOnInit() {
     this.getDetails.mymodelDetails().subscribe(result => {
-    console.log('login check point result - ', result);
+      this.projects=result;
+      this.filteredproject = this.projects;
+
+      console.log('login check point result - ', result);
      });
+
+
+
+
   }
 }
