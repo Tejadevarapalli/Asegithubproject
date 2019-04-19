9ddcb6a6351f0cba7b12b779787ec151f7f8d8f4
rahuldhar123 <rahuldhamerla@gmail.com>
2019-04-18 23:26:41 -0500

    View Changes

--- backend/frontend/src/app/view/view.component.ts
+++ backend/frontend/src/app/view/view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap} from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import {mongoService} from '../mongo.service';
+import {any} from "codelyzer/util/function";
 
 @Component({
   selector: 'app-view',
@@ -9,13 +10,14 @@ import {mongoService} from '../mongo.service';
   styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
-
+public value= any;
   constructor(private route: ActivatedRoute, public service: mongoService) { }
   ngOnInit() {
     let id= this.route.snapshot.paramMap.get('id');
     console.log(id);
     this.service.viewdetails(id).subscribe(result => {
     console.log(result);
+    this.value=result;
     });
   }
 
