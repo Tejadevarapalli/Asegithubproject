9aa65dedebd43ee1612795a01f11167fb46a63c8
rahuldhar123 <rahuldhamerla@gmail.com>
2019-04-19 12:34:16 -0500

    changes

--- backend/frontend/src/app/mymodels/mymodels.component.ts
+++ backend/frontend/src/app/mymodels/mymodels.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+  import { Component, OnInit } from '@angular/core';
 import {mongoService} from '../mongo.service';
 import {IProject} from './project';
 import { Router, ActivatedRoute, ParamMap} from '@angular/router';
